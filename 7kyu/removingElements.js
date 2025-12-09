5edd6686533f2ba4bdb536e724a988173a6d33ff
Joyce Donkor <teiscalia@gmail.com>
2025-12-09 13:47:20 -0500

    2nd Solution added

--- 7kyu/removingElements.js
+++ 7kyu/removingElements.js
@@ -13,7 +13,17 @@ function removeEveryOther(arr) {
   });
 }
 
+//2nd Solution added
+
+// function removeEveryOther(arr) {
+//   var newArr = [];
+//   for (var i = 0; i < arr.length; i += 2) {
+//     newArr.push(arr[i]);
+//   }
+//   return newArr;
+// }
+
 // removeEveryOther(["Hello", "Goodbye", "Hello Again"]);
 
-// console.log(removeEveryOther(["Hello", "Goodbye", "Hello Again"]));
-console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(removeEveryOther(["Hello", "Goodbye", "Hello Again"]));
+// console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
